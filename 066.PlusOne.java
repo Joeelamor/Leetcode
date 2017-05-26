d4d8628358407ab7146230dff9657d66d614680a
Joeelamor <xiaozhu930330@gmail.com>
2017-05-26 00:03:54 -0500

    Minor fix.

--- 066.PlusOne.java
+++ 066.PlusOne.java
@@ -6,12 +6,12 @@ public class Solution {
         for(int i = m - 1; i >= 0; i--) {
             if(digits[i] < 9) {
                 digits[i]++;
-                return digits;   //if there is no carry in last digit after 
-                					plussing, then return digits.
+                return digits;      
+                //if there is no carry in last digit after plussing, then return digits.
             }
             else
-                digits[i] = 0;   //if there is carry, then look at previous 
-                					digit.
+                digits[i] = 0;      
+                //if there is carry, then look at previous digit.
         }
         int[] result = new int[m+1];
         result[0] = 1;
