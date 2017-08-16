fdd99c33d4cb1231a3b6ae2e9c74fd2f56e2dabf
Joeelamor <xiaozhu930330@gmail.com>
2017-08-16 16:32:47 -0500

    Change idea: use number as index

---
+++ 448.FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,18 @@
+// Problem 448 Find All Numbers Disappeared in an Array
+
+// Use number as index.
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if(nums[val] > 0)
+                nums[val] = -nums[val];
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0)
+                list.add(i + 1);
+        }
+        return list;
+    }
+}
