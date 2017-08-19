38ace1fe8fa0f9df156fa3d904e0ed54426e8355
Joeelamor <xiaozhu930330@gmail.com>
2017-08-19 17:33:41 -0500

    Add a method

--- java/053.MaximumSubarray.java
+++ java/053.MaximumSubarray.java
@@ -13,3 +13,19 @@ public class Solution {
         return maxSum;
     }
 }
+
+// Method 2:
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(sum < 0)
+                sum = nums[i];
+            else
+                sum += nums[i];
+            if(max < sum)
+                max = sum;
+        }
+        return max;
+    }
+}
