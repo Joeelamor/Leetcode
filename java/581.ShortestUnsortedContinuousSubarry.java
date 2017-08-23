6f6ce22e855940363d2a69e8bcfa0dcbba005a75
Joeelamor <xiaozhu930330@gmail.com>
2017-08-23 13:08:56 -0500

    Add a new method

--- java/581.ShortestUnsortedContinuousSubarry.java
+++ java/581.ShortestUnsortedContinuousSubarry.java
@@ -16,3 +16,19 @@ class Solution {
         return end - start + 1;
     }
 }
+
+// Method 2: O(n) time
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length, start = -1, end = -2, min = nums[n -1], max = nums[0];
+        for(int i = 1; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - i - 1]);
+            if(nums[i] < max)
+                end = i;
+            if(nums[n - i - 1] > min)
+                start = n - i - 1;
+        }
+        return end - start + 1;
+    }
+}
