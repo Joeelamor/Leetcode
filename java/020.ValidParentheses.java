0ee1b0244a9e1266e0f1686d40bd3531e06de2e2
Joeelamor <xiaozhu930330@gmail.com>
2017-08-24 21:10:22 -0500

    Add a new method

--- java/020.ValidParentheses.java
+++ java/020.ValidParentheses.java
@@ -19,3 +19,23 @@ public class Solution {
         return stack.isEmpty();
     }
 }
+
+// My universal method
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else if(c == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if(c == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else if(c == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else
+                return false;
+        }
+        return stack.empty();
+    }
+}
