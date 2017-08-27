9520ba7c5d3c06dc4ad7cb25fd4e4667ace793a1
Joeelamor <xiaozhu930330@gmail.com>
2017-08-27 16:49:20 -0500

    Add a recursion method

--- java/206.ReverseLinkedList.java
+++ java/206.ReverseLinkedList.java
@@ -46,3 +46,25 @@ public class Solution {
         return newHead;
     }
 }
+
+// Use recursion
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+    public ListNode reverse(ListNode head, ListNode newHead) {
+        if(head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverse(next, head);
+    }
+}
