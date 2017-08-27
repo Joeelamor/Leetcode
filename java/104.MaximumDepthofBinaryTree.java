eecf8223bdeab47376f72f71d9f0ba279d20ca18
Joeelamor <xiaozhu930330@gmail.com>
2017-08-27 14:05:48 -0500

    BFS and DFS

--- java/104.MaximumDepthofBinaryTree.java
+++ java/104.MaximumDepthofBinaryTree.java
@@ -1,5 +1,6 @@
 // Problem 104 Maximum Depth of Binary Tree
 
+// Use DFS
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,5 +15,37 @@ public class Solution {
         if(root == null)
             return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+// Use BFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+                while(size-- > 0) {
+                    TreeNode node = queue.poll();
+                    if(node.left != null)
+                        queue.offer(node.left);
+                    if(node.right != null)
+                        queue.offer(node.right);                       
+                }
+            count++;
+        }
+        return count;
     }
 }
