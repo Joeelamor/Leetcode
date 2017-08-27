4404c57a7516af77e3ef752fd1b7d3be15989e33
Joeelamor <xiaozhu930330@gmail.com>
2017-08-27 15:51:41 -0500

    Add a method

--- java/283.MoveZeroes.java
+++ java/283.MoveZeroes.java
@@ -13,3 +13,22 @@ public class Solution {
         }
     }
 }
+
+// An easily understood method
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return;
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while(j <= nums.length - 1) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+}
