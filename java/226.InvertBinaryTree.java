a48d8c2a64d029b87d847cdea415ccf72af65752
Joeelamor <xiaozhu930330@gmail.com>
2017-08-27 15:31:35 -0500

    Add DFS and BFS details

--- java/226.InvertBinaryTree.java
+++ java/226.InvertBinaryTree.java
@@ -1,5 +1,6 @@
 // Problem 226 Invert Binary Tree
 
+// Use DFS recursion
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -18,5 +19,65 @@ public class Solution {
         root.right = invertTree(tmp);
         return root;
         
+    }
+}
+
+// Use DFS stack
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) 
+            return null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.empty()) {
+            TreeNode node = stack.pop();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if(node.left != null)
+                stack.push(node.left);
+            if(node.right != null)
+                stack.push(node.right);
+        }
+        return root;
+    }
+}
+
+// Use BFS Queue
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if(node.left != null)
+                q.offer(node.left);
+            if(node.right != null)
+                q.offer(node.right);
+        }
+        return root;
     }
 }
