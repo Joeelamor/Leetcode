3a1c81e2288f427c747cfd76ebe1bf2f30fdc42a
Joeelamor <xiaozhu930330@gmail.com>
2017-08-29 17:35:56 -0500

    Add a basic idea.

--- java/056.MergeIntervals.java
+++ java/056.MergeIntervals.java
@@ -1,5 +1,6 @@
 // Problem 56 Merge Intervals
 
+// Method 1: sort starts and ends at the same time
 /**
  * Definition for an interval.
  * public class Interval {
@@ -28,5 +29,43 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+// Method 2: only sort starts, method 1 is a more succinct based on method 2.
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() <= 1)
+        return intervals;
+    
+        // Sort by ascending starting point using an anonymous Comparator
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        List<Interval> result = new LinkedList<Interval>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (Interval interval : intervals) {
+            if (interval.start <= end) // Overlapping intervals, move the end if needed
+                end = Math.max(end, interval.end);
+            else {                     // Disjoint intervals, add the previous one and reset bounds
+                result.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+
+        // Add the last interval
+        result.add(new Interval(start, end));
+        return result;
     }
 }
