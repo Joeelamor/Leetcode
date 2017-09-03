0a61a5b92faa042f9e79c7056aa95353702293a0
Joeelamor <xiaozhu930330@gmail.com>
2017-09-03 09:20:23 -0500

    Contest 671

---
+++ java/671.SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,37 @@
+// Problem 671 Second Minimum Node In A Binary Tree
+
+// A stupid method: store all number in a ArrayList and sort it, then find the second smallest one
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        HashSet<Integer> set = new HashSet<>();
+        help(root, set);
+        List<Integer> lst = new ArrayList(set);
+        Collections.sort(lst);
+        if (lst.size() < 2) {
+            return -1;
+        } else {
+            return lst.get(1);
+        }
+    }
+    
+    private void help(TreeNode node, HashSet<Integer> set) {
+        if (node == null) {
+            return;
+        }
+        
+        set.add(node.val);
+        help(node.left, set);
+        help(node.right, set);
+    }
+}
+
+// Method 2:
