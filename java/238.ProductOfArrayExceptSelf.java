d3a9e93f4b05f1fbf6dbbe2ce48ecdac7a267788
Joeelamor <xiaozhu930330@gmail.com>
2017-09-04 15:44:29 -0500

    New post

---
+++ java/238.ProductOfArrayExceptSelf.java
@@ -0,0 +1,19 @@
+// Problem 238 Product Of Array Except Self
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int []res = new int[nums.length];
+        res[0] = 1;
+        // Use an array to record all product of numbers before each index.
+        for(int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        // Reversely traverse the array to calculate the result.
+        for(int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
