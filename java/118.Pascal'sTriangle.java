3d3bdaf5d06f23c254843fbe1bdd5acbcaa7ca41
Joeelamor <xiaozhu930330@gmail.com>
2017-09-06 17:35:40 -0500

    Upload a new solution

--- java/118.Pascal'sTriangle.java
+++ java/118.Pascal'sTriangle.java
@@ -24,9 +24,24 @@ class Solution {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         List<Integer> row = new ArrayList<>();
         for(int i = 0; i < numRows; i++) {
-            row.add(1);
+            row.add(1); // Add 1 at the end.
             for(int j = row.size() - 2; j > 0; j--) {
-                row.set(j, row.get(j) + row.get(j - 1));
+                row.set(j, row.get(j) + row.get(j - 1)); // From right to left.
+            }
+            res.add(new ArrayList<Integer>(row));
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> row = new ArrayList<>();
+        for(int i = 0; i < numRows; i++) {
+            row.add(0, 1); // Set the first number 1.
+            for(int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1)); // From left to right.
             }
             res.add(new ArrayList<Integer>(row));
         }
