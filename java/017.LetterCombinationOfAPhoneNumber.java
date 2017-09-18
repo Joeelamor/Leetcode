1d5b9a1ede167d7ab6ce9a2fc2f0872e129a2e09
Joeelamor <xiaozhu930330@gmail.com>
2017-09-17 21:26:44 -0500

    Upload new solution

---
+++ java/017.LetterCombinationOfAPhoneNumber.java
@@ -0,0 +1,22 @@
+// Problem 17 Letter Combination Of A Phone Number
+
+class Solution {
+    public String[] keys = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if(digits.length() == 0)
+            return res;
+        combination("", digits, 0, res);
+        return res;
+    }
+    public void combination(String prefix, String digits, int offset, List<String> res) {
+        if(offset >= digits.length()) {
+            res.add(prefix);
+            return;
+        }
+        String letter = keys[(digits.charAt(offset) - '0')];
+        for(int i = 0; i < letter.length(); i++) {
+            combination(prefix + letter.charAt(i), digits, offset + 1, res);
+        }
+    }
+}
