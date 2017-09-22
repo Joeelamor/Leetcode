13e2f0c2df8aeabcd397cf6862af6bb5724639bd
Joeelamor <xiaozhu930330@gmail.com>
2017-09-22 15:31:08 -0500

    Add a new method

--- java/121.BestTimetoBuyAndSellStock.java
+++ java/121.BestTimetoBuyAndSellStock.java
@@ -28,3 +28,22 @@ class Solution {
         return max;
     }
 }
+
+// Other method:
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
+        int min = prices[0], profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else {
+                if (prices[i] - min > profit) {
+                    profit = prices[i] - min;
+                }
+            }
+        }
+        return profit;
+    }
+}
