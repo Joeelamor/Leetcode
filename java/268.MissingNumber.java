d0588be5a97f441c78c2b244805c97cf50ef9df6
Joeelamor <xiaozhu930330@gmail.com>
2017-09-21 22:28:07 -0500

    Upload Problem 268

---
+++ java/268.MissingNumber.java
@@ -0,0 +1,11 @@
+// Problem 268 Missing Number
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return (nums.length + 1) * nums.length / 2 - sum;
+    }
+}
