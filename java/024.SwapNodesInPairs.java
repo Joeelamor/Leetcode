43e2dab843a5b931ce6f254109ca4122c74267bb
Joeelamor <xiaozhu930330@gmail.com>
2017-09-25 20:46:23 -0500

    Post a new solution

---
+++ java/024.SwapNodesInPairs.java
@@ -0,0 +1,47 @@
+// Problem 24 Swap Nodes in Pairs
+
+// Method 1: use iteration
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode res = new ListNode(0);
+        res.next = head;
+        ListNode current = res;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return res.next;
+    }
+}
+
+// Method 2: Use recursion
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode res = head.next;
+        head.next = swapPairs(head.next.next);
+        res.next = head;
+        return res;
+    }
+}
