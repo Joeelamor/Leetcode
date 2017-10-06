4d501de1e321f27a7df4bc67a9dd27be36584022
Joeelamor <xiaozhu930330@gmail.com>
2017-10-06 16:13:29 -0500

    Add a new method

--- java/069.Sqrt.java
+++ java/069.Sqrt.java
@@ -12,3 +12,18 @@ public class Solution {
     }
 }
 
+// Method 2:
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1, right = x, result = 0, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                result = mid;
+            } else 
+                right = mid - 1;
+        }
+        return result;
+    }
+}
