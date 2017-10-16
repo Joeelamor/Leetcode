d7d435fd4da8ae5557d4b24a9e858013405d071f
Joeelamor <xiaozhu930330@gmail.com>
2017-10-15 20:52:09 -0500

    Backtracking problem

---
+++ java/046.Permutations.java
@@ -0,0 +1,21 @@
+// Problem 46 Permutations
+
+// Use backtracking
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backTracking(nums, res, new ArrayList<>());
+        return res;
+    }
+    public void backTracking(int[] nums, List<List<Integer>> res, List<Integer> list) {
+        if (list.size() == nums.length)
+            res.add(new ArrayList<>(list));
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i]))
+                continue;
+            list.add(nums[i]);
+            backTracking(nums, res, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
