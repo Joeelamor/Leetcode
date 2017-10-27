80a2961ccb215adadca614ccb197a0b6ae4ef235
Joeelamor <xiaozhu930330@gmail.com>
2017-10-27 18:48:04 -0500

    Add a new method

--- java/075.SortColors.java
+++ java/075.SortColors.java
@@ -1,5 +1,6 @@
 // Problem 75 Sort Colors
 
+// Two pass
 class Solution {
     public void sortColors(int[] nums) {
         int count0 = 0, count1 = 0, count2 = 0;
@@ -18,6 +19,27 @@ class Solution {
                 nums[i] = 1;
             else
                 nums[i] = 2;
+        }
+    }
+}
+
+// One pass
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0, right = nums.length - 1, index = 0;
+        while (index <= right) {
+            if (nums[index] == 0) {
+                nums[index] = nums[left];
+                nums[left] = 0;
+                left++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[right];
+                nums[right] = 2;
+                right--;
+                index--;
+            }
+            index++;
         }
     }
 }
