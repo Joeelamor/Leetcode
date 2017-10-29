69061b81aa9fc209130621c3355b37e3a2514366
Joeelamor <xiaozhu930330@gmail.com>
2017-10-28 22:06:04 -0500

    Add a method

--- java/717.1-bitAnd2-bitCharacters.java
+++ java/717.1-bitAnd2-bitCharacters.java
@@ -1,5 +1,6 @@
 // Problem 717 1-bit and 2-bit Characters
 
+// Method 1: 
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         if (bits.length == 1) {
@@ -14,5 +15,20 @@ class Solution {
             return isOneBitCharacter(Arrays.copyOfRange(bits, i + 2, bits.length));
         }
         return false;
+    }
+}
+
+
+// Method 2 :
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        boolean ret = true;
+        for (int i = 0; i < bits.length - 1; i++) {
+            if (ret && bits[i] == 1)
+                ret = false;
+            else if (ret == false)
+                ret = true;
+        }
+        return ret;
     }
 }
