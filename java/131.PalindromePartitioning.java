46d60692501f6068e1c1c3d66bb24481bd0c3f36
Joeelamor <xiaozhu930330@gmail.com>
2017-11-07 21:27:26 -0600

    Add a method

--- java/131.PalindromePartitioning.java
+++ java/131.PalindromePartitioning.java
@@ -11,8 +11,10 @@ class Solution {
         return res;
     }
     public void partition(String s, List<List<String>> res, List<String> list, int start) {
-        if (list.size() > 0 && start >= s.length())
+        if (list.size() > 0 && start >= s.length()) {
             res.add(new ArrayList<String>(list));
+            return;
+        }
         for (int i = start; i < s.length(); i++) {
             if (isPalindrom(s.substring(start, i + 1))) {
                 list.add(s.substring(start, i + 1));
@@ -36,5 +38,34 @@ class Solution {
         return true;
     }
 }
+}
 
 // Method 2: use DP
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        boolean [][]dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1]))
+                    dp[j][i] = true;
+            }
+        }
+        partition(res, dp, new ArrayList<>(), s, 0);
+        return res;
+    }
+    public void partition(List<List<String>> res, boolean [][]dp, List<String> list, String s, int start) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (dp[start][i]) {
+                list.add(s.substring(start, i + 1));
+                partition(res, dp, list, s, i + 1);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
