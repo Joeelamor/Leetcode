a13f7e3412e5940c7e721ecbed9b5e07c8cc2e9b
Joeelamor <xiaozhu930330@gmail.com>
2017-11-09 16:56:32 -0600

    Add a method

--- java/713.SubarrayProductLessThanK.java
+++ java/713.SubarrayProductLessThanK.java
@@ -19,3 +19,18 @@ class Solution {
         return count;
     }
 }
+
+// method 2: sliding window
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k) prod /= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+}
