c9b259a785c764435f64c4c0333f1d53421b0882
Joeelamor <xiaozhu930330@gmail.com>
2017-11-09 16:39:59 -0600

    Add a method

--- java/198.HouseRobber.java
+++ java/198.HouseRobber.java
@@ -4,6 +4,7 @@
 // number plus last max even number with last max odd number. And for odd number,
 // we have same comparison.
 
+// Method 1:
 public class Solution {
     public int rob(int[] nums) {
         int a = 0, b = 0;
@@ -14,5 +15,17 @@ public class Solution {
                 b = Math.max(a, b + nums[i]);
         }
         return Math.max(a, b);
+    }
+}
+
+// Method 2: DP
+class Solution {
+    public int rob(int[] nums) {
+        int [][]dp = new int[nums.length + 1][2];
+        for (int i = 1; i < nums.length + 1; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + nums[i - 1];
+        }
+        return Math.max(dp[nums.length][0], dp[nums.length][1]);
     }
 }
