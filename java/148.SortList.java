d5dde3b6d56cffc71df6d64edea3cd1fa43f9ebd
Joeelamor <xiaozhu930330@gmail.com>
2017-11-09 15:56:09 -0600

    Merge sort List

---
+++ java/148.SortList.java
@@ -0,0 +1,47 @@
+// Problem 148 Sort List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode pre = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        // divide
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        // sort and conquer
+        return merge(l1, l2);
+    }
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null)
+            cur.next = l1;
+        if (l2 != null)
+            cur.next = l2;
+        return dummy.next;
+    }
+}
