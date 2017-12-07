80f56e65f6e88f1c838bc3e0d4edbc7a5c33fed2
Joeelamor <xiaozhu930330@gmail.com>
2017-12-07 17:04:42 -0600

    Problem 583

--- java/583.DeleteOperationForTwoStrings.java
+++ java/583.DeleteOperationForTwoStrings.java
@@ -29,14 +29,10 @@ class Solution {
         int [][]dp = new int[word1.length() + 1][word2.length() + 1];
         for(int i = 1; i <= word1.length(); i++) {
             for(int j = 1; j <= word2.length(); j++) {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else {
-                    if(word1.charAt(i - 1) == word2.charAt(j - 1))
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    else
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }                
+                if(word1.charAt(i - 1) == word2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);               
             }
         }
         return word1.length() + word2.length() - 2 * dp[word1.length()][word2.length()];
