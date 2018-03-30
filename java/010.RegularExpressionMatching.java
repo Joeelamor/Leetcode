0ab0c3a42a5e300cb5deea60381710d262e2facf
Joeelamor <lizhongzhang330@gmail.com>
2018-03-29 22:43:25 -0500

    Add a new method

--- java/010.RegularExpressionMatching.java
+++ java/010.RegularExpressionMatching.java
@@ -33,6 +33,38 @@ class Solution {
     }
 }
 
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < dp[0].length; j++) {
+            if (p.charAt(j - 1) == '*') {
+                if (dp[0][j - 1] || (j > 1 && dp[0][j - 2])) {
+                    dp[0][j] = true;
+                }
+            } 
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    }
+                }         
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
+
 // Method 2: use one-dimension array
 class Solution {
     public boolean isMatch(String s, String p) {
