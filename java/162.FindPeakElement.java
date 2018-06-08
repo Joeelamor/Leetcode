a2c1dc21eadba32ea7fa60f51a31c46395f73c73
Joeelamor <lizhongzhang330@gmail.com>
2018-06-07 20:17:57 -0500

    Problem 162

---
+++ java/162.FindPeakElement.java
@@ -0,0 +1,27 @@
+// Problem 162 Find Peak Element
+
+// Method 1: O(n)
+class Solution {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+}
+
+// Method 2: Use Binary Search
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
