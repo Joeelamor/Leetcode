a0f837495e6e629e73f7f5b1a87b7a21569fdc27
Joeelamor <lizhongzhang330@gmail.com>
2018-06-07 20:09:51 -0500

    Update new code

--- java/081.SearchInRotatedSortedArrayII.java
+++ java/081.SearchInRotatedSortedArrayII.java
@@ -3,25 +3,27 @@
 // If there will be repeated numbers in array, just split it into two parts, then consider all edge cases.
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, mid;
-        while (left <= right) {
-            mid = (right + left) / 2;
-            if (nums[mid] == target)
+        if(nums == null || nums.length == 0) return false;
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            int mid = (right + left) / 2;
+            if(nums[mid] == target)
                 return true;
-            if (nums[mid] < nums[left] || nums[mid] < nums[right]) {
-                if (target <= nums[right] && target > nums[mid]) {
+            if(nums[left] < nums[mid]) {
+                if(target >= nums[left] && target < nums[mid])
+                    right = mid;
+                else
                     left = mid + 1;
-                } else
-                    right = mid - 1;
+            } else if (nums[mid] < nums[left]){
+                if(target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid;
+            } else {
+                left++;
             }
-            else if (nums[mid] > nums[left] || nums[mid] > nums[right]) {
-                if (target < nums[mid] && target >= nums[left]) {
-                    right = mid - 1;
-                } else
-                    left = mid + 1;
-            } else
-                right--;
         }
-        return false;
+        return nums[left] == target? true : false;
     }
 }
+
