1ea8f582bffd7e9942788e02fa7fab30152b323c
Joeelamor <lizhongzhang330@gmail.com>
2018-06-14 19:59:44 -0500

    Problem 229

---
+++ java/229.MajorityElementII.java
@@ -0,0 +1,42 @@
+// Problem 229 Majority Element II
+
+// Boyer-Moore Majority Vote Algorithm. Find two magority values.
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums.length == 0 || nums == null) return res;
+        int num1 = nums[0], num2 = nums[0], count1 = 0, count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            } else if (count1 == 0) {
+                num1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                num2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            }
+        }
+        if (count1 > nums.length / 3) {
+            res.add(num1);
+        }
+        if (count2 > nums.length / 3) {
+            res.add(num2);
+        }
+        return res;
+    }
+}
