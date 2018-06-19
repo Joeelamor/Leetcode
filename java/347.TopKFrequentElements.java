a10724593316d24c4d6c228d801d71c43c3c9f93
Joeelamor <lizhongzhang330@gmail.com>
2018-06-18 22:00:13 -0500

    Problem 347

---
+++ java/347.TopKFrequentElements.java
@@ -0,0 +1,26 @@
+// Problem 347 Top K Frequent Elements 
+
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (bucket[freq] == null) bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = bucket.length - 1; i > 0 && k > 0; i--) {
+            if (bucket[i] != null) {
+                List<Integer> list = bucket[i];
+                for (int n : list) {
+                    res.add(n);
+                    k--;
+                    if (k == 0) break;
+                }
+            }
+        }
+        return res;
+    }
+}
