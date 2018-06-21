e681f9da444c3db7bb44e405840a4a2b9f153243
Joeelamor <lizhongzhang330@gmail.com>
2018-06-20 23:34:59 -0500

    Problem 299

---
+++ java/299.BullsAndCows.java
@@ -0,0 +1,16 @@
+// Problem 299 Bulls And Cows
+
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] nums = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else {
+                if (nums[secret.charAt(i) - '0']++ < 0) cows++;
+                if (nums[guess.charAt(i) - '0']-- > 0) cows++;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
