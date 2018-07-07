d76ecc68c4851f3a043646eb1a795a4791940324
Joeelamor <lizhongzhang330@gmail.com>
2018-07-06 20:48:47 -0500

    Problem 384

---
+++ java/384.ShuffleAnArray.java
@@ -0,0 +1,39 @@
+// Problem 384 Shuffle An Array
+
+class Solution {
+    private int[] nums;
+    private Random rand;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        rand = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if (nums == null) return null;
+        int[] a = nums.clone();
+        for (int i = 1; i < a.length; i++) {
+            int j = rand.nextInt(i + 1);
+            swap(a, i, j);
+        }
+        return a;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
