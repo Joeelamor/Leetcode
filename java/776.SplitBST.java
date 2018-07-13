073f9ab91d9842df10552f592dc5c5afb81f006d
Joeelamor <lizhongzhang330@gmail.com>
2018-07-12 20:27:47 -0500

    Problem 776

---
+++ java/776.SplitBST.java
@@ -0,0 +1,27 @@
+// Problem 776 Split BST
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode[] splitBST(TreeNode root, int V) {
+        if (root == null) return new TreeNode[]{null, null};
+        TreeNode[] res;
+        if (root.val <= V) {
+            res = splitBST(root.right, V);
+            root.right = res[0];
+            res[0] = root;
+        } else {
+            res = splitBST(root.left, V);
+            root.left = res[1];
+            res[1] = root;
+        }
+        return res;
+    }
+}	
