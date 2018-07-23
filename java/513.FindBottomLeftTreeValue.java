a5ca85f81c487b0dd80750c04649cb1bea9b33c9
Joeelamor <lizhongzhang330@gmail.com>
2018-07-22 21:13:15 -0500

    Add a new method

--- java/513.FindBottomLeftTreeValue.java
+++ java/513.FindBottomLeftTreeValue.java
@@ -26,3 +26,31 @@ class Solution {
         return arr[0];
     }
 }
+
+
+// method 2: bfs
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            root = q.poll();
+            if (root.right != null)
+                q.offer(root.right);
+            if (root.left != null)
+                q.offer(root.left);
+        }
+        return root.val;
+    }
+}
