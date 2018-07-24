c211ab8e418b1713d066a2330caf95ae330e3b61
Joeelamor <lizhongzhang330@gmail.com>
2018-07-23 20:24:38 -0500

    Problem 704

---
+++ java/704.BinarySearch.java
@@ -0,0 +1,18 @@
+// Problem 704 Binary Search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
