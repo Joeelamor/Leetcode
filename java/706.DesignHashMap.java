7272e50938ea21f3cd046737360f2f306c9cfaec
Joeelamor <lizhongzhang330@gmail.com>
2018-07-27 20:57:28 -0500

    Change the method

--- java/706.DesignHashMap.java
+++ java/706.DesignHashMap.java
@@ -1,68 +1,88 @@
 // Problem 706 Design HashMap
 
-class MyHashMap {
+class HashItem {
+    int key, val;
+    HashItem next;
+    HashItem(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+public class MyHashMap {
     
-    final Bucket[] buckets;
+    private HashItem[] list;
+    private int size;
+
     /** Initialize your data structure here. */
     public MyHashMap() {
-        buckets = new Bucket[10000];
+        list = new HashItem[16];
     }
     
-    /** value will always be non-negative. */
+    /** value will always be positive. */
     public void put(int key, int value) {
-        int i = findIndex(key);
-        if (buckets[i] == null)
-            buckets[i] = new Bucket();
-        ListNode prev = find(buckets[i], key);
-        if (prev.next == null)
-            prev.next = new ListNode(key, value);
-        else
-            prev.next.val = value;
+        int hash = key % list.length;
+        if (list[hash] == null) {
+            list[hash] = new HashItem(key, value);
+        }
+        else if (list[hash].key == key) {
+            list[hash].val = value;
+        }
+        else {
+            HashItem curr = list[hash];
+            while (curr.next != null && curr.next.key != key) 
+                curr = curr.next;
+            if (curr.next != null) 
+                curr.next.val = value;
+            else
+                curr.next = new HashItem(key, value);
+        }
+        size += 1;
+        if (size * 1.0 / list.length > 0.8) {
+            rehash();
+        }
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        int i = findIndex(key);
-        if (buckets[i] == null)
-            return -1;
-        ListNode prev = find(buckets[i], key);
-        return prev.next == null ? -1 : prev.next.val;
+        int hash = key % list.length;
+        HashItem curr = list[hash];
+        while (curr != null) {
+            if (curr.key == key)
+                return curr.val;
+            curr = curr.next;
+        }
+        return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        int i = findIndex(key);
-        if (buckets[i] == null) return;
-        ListNode prev = find(buckets[i], key);
-        if (prev.next == null) return;
-        prev.next = prev.next.next;
-    }
-    
-    private int findIndex(int key) {
-        return Integer.hashCode(key) % buckets.length;
-    }
-    
-    private ListNode find(Bucket bucket, int key) {
-        ListNode node = bucket.head, prev = null;
-        while (node != null && node.key != key) {
-            prev = node;
-            node = node.next;
+        int hash = key % list.length;
+        if (list[hash] == null) {
+            return;
         }
-        return prev;
+        else if (list[hash].key == key) {
+            list[hash] = list[hash].next;
+        }
+        else {
+            HashItem curr = list[hash];
+            while (curr.next != null && curr.next.key != key) 
+                curr = curr.next;
+            if (curr.next != null) 
+                curr.next = curr.next.next;
+        }
     }
-}
-
-class Bucket {
-    final ListNode head = new ListNode(-1, -1);
-}
-
-class ListNode {
-    int key;
-    int val;
-    ListNode next;
-    ListNode (int key, int val) {
-        this.key = key;
-        this.val = val;
+    
+    private void rehash() {
+        HashItem[] oldList = list;
+        list = new HashItem[list.length * 2];
+        for (int i = 0; i < oldList.length; i++) {
+            HashItem curr = oldList[i];
+            while (curr != null) {
+                put(curr.key, curr.val);
+                curr = curr.next;
+            }
+        }
     }
 }
 
