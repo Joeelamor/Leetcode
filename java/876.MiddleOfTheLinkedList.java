1b14a46b90720b4600328bbe57ba3705c0aaf713
Joeelamor <lizhongzhang330@gmail.com>
2018-07-29 13:10:22 -0500

    Problem 876

---
+++ java/876.MiddleOfTheLinkedList.java
@@ -0,0 +1,20 @@
+// Problem 876 Middle Of The Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast.next == null ? slow : slow.next;
+    }
+}
