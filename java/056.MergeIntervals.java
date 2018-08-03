958959e75ac96affea8347a39e5725e2a4de5672
Joeelamor <lizhongzhang330@gmail.com>
2018-08-02 20:26:26 -0500

    Add comparator function

--- java/056.MergeIntervals.java
+++ java/056.MergeIntervals.java
@@ -69,3 +69,36 @@ class Solution {
         return result;
     }
 }
+
+
+// override comparator
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() <= 1)
+        return intervals;
+    
+        Collections.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+
+        List<Interval> result = new LinkedList<Interval>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (Interval interval : intervals) {
+            if (interval.start <= end) // Overlapping intervals, move the end if needed
+                end = Math.max(end, interval.end);
+            else {                     // Disjoint intervals, add the previous one and reset bounds
+                result.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+
+        // Add the last interval
+        result.add(new Interval(start, end));
+        return result;
+    }
+}
