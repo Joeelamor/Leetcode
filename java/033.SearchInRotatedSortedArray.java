ed31260476b62c8ed46a380e6ab6a851d7c1ce96
Joeelamor <lizhongzhang330@gmail.com>
2018-09-22 12:03:52 -0500

    Add new method

--- java/033.SearchInRotatedSortedArray.java
+++ java/033.SearchInRotatedSortedArray.java
@@ -14,25 +14,25 @@ class Solution {
 // Method 2: O(logn) time
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
+        if (nums.length == 0)
+          return -1;
         int left = 0, right = nums.length - 1;
-        while(left < right) {
-            int mid = (right + left) / 2;
-            if(nums[mid] == target)
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
                 return mid;
-            // Find continuous ascending array
-            if(nums[left] <= nums[mid]) {
-                if(target >= nums[left] && target < nums[mid])
-                    right = mid - 1;
-                else
+            if (nums[mid] > target) {
+                if (nums[mid] >= nums[left] && nums[left] > target)
                     left = mid + 1;
+                else
+                    right = mid - 1;
             } else {
-                if(target > nums[mid] && target <= nums[right])
-                    left = mid + 1;
-                else
+                if (nums[mid] <= nums[right] && nums[right] < target) 
                     right = mid - 1;
+                else
+                    left = mid + 1;
             }
         }
-        return nums[left] == target? left: -1;
+        return nums[left] == target? left : -1;
     }
 }
--- java/081.SearchInRotatedSortedArrayII.java
+++ java/081.SearchInRotatedSortedArrayII.java
@@ -3,27 +3,28 @@
 // If there will be repeated numbers in array, just split it into two parts, then consider all edge cases.
 class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return false;
+        if (nums.length == 0)
+          return false;
         int left = 0, right = nums.length - 1;
-        while(left < right) {
-            int mid = (right + left) / 2;
-            if(nums[mid] == target)
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
                 return true;
-            if(nums[left] < nums[mid]) {
-                if(target >= nums[left] && target < nums[mid])
-                    right = mid;
-                else
-                    left = mid + 1;
-            } else if (nums[mid] < nums[left]){
-                if(target > nums[mid] && target <= nums[right])
+            if ((nums[left] == nums[mid]) && (nums[right] == nums[mid])) {
+                ++left; 
+                --right;
+            } else if (nums[mid] > target) {
+                if (nums[mid] >= nums[left] && nums[left] > target)
                     left = mid + 1;
                 else
-                    right = mid;
+                    right = mid - 1;
             } else {
-                left++;
+                if (nums[mid] <= nums[right] && nums[right] < target) 
+                    right = mid - 1;
+                else
+                    left = mid + 1;
             }
         }
         return nums[left] == target? true : false;
     }
 }
-
