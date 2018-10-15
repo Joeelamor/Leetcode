0ccf9b304cda5a41d93d47cf5f91db9758856dea
Joeelamor <lizhongzhang330@gmail.com>
2018-10-14 21:16:54 -0500

    Question 354

---
+++ java/354.RussianDollEnvelopes.java
@@ -0,0 +1,23 @@
+// Question 354 Russian Doll Envelopes
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        int[] dp = new int[envelopes.length];
+        Arrays.fill(dp, 1);
+        int max = 0;
+        for (int i = 0; i < envelopes.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
