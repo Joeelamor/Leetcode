6c867c89e880f9b55488f3a87d8f4e3b1f57fa79
Joeelamor <lizhongzhang330@gmail.com>
2018-10-15 14:12:51 -0500

    Add a new method

--- java/146.LRUCache.java
+++ java/146.LRUCache.java
@@ -38,3 +38,89 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+// Method 2: implement LinkedHashMap
+
+class LRUCache {
+    class Node {
+        Node prev, next;
+        int key;
+        int val;
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+        Node() {
+            this(0, 0);
+        }
+    }
+    
+    int capacity;
+    int count;
+    HashMap<Integer, Node> map;
+    Node head, tail;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.count = 0;
+        this.map = new HashMap<>();
+        this.head = new Node();
+        this.tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        Node node = map.get(key);
+        if (node == null)
+            return -1;
+        update(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        Node node = map.get(key);
+        if (node == null) {
+            node = new Node(key, value);
+            map.put(key, node);
+            add(node);
+            count++;
+        } else {
+            node.val = value;
+            update(node);
+        }
+        if (count > capacity) {
+            Node del = tail.prev;
+            remove(del);
+            map.remove(del.key);
+            count--;
+        }
+    }
+    
+    public void update(Node node) {
+        remove(node);
+        add(node);
+    }
+    
+    public void add(Node node) {
+        Node after = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = after;
+        after.prev = node;
+    }
+    
+    public void remove(Node node) {
+        Node before = node.prev;
+        Node after = node.next;
+        before.next = after;
+        after.prev = before;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
