db15575671583145a59495968511ac22b6856620
Joeelamor <lizhongzhang330@gmail.com>
2018-10-27 10:58:16 -0500

    Add a new method

--- java/215.KthLargestElementInAnArray.java
+++ java/215.KthLargestElementInAnArray.java
@@ -28,3 +28,52 @@ class Solution {
         return pq.peek();        
     }
 }
+
+
+// Method 3: quick select
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) 
+            return -1;
+        return findKthLargest(nums, 0, nums.length - 1, k);
+    }
+    
+    int findKthLargest(int[] nums, int start, int end, int k) {
+        if (start == end) {
+            return nums[start];
+        }
+        
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+        
+        int left = start, right = end;
+        while (left <= right) {
+            while (left <= right && nums[left] > pivot) {
+                left++;
+            }
+            
+            while (left <= right && nums[right] < pivot) {
+                right--;
+            }
+            
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        if (right - start + 1 >= k) {
+            return findKthLargest(nums, start, right, k);
+        }
+        
+        if (k > left - start) {
+            return findKthLargest(nums, left, end, k - (left - start));
+        }
+        
+        return nums[right + 1];
+    }
+}
