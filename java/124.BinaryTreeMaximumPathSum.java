227bf4b69bbf052aeb29ea61fe996315f50aa15a
Joeelamor <lizhongzhang330@gmail.com>
2018-10-31 10:32:06 -0500

    Add a method to record path

--- java/124.BinaryTreeMaximumPathSum.java
+++ java/124.BinaryTreeMaximumPathSum.java
@@ -24,3 +24,55 @@ class Solution {
         return Math.max(left, right) + node.val;
     }
 }
+
+// Record Path
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int max = Integer.MIN_VALUE;
+    List<Integer> res = new ArrayList<>();
+    public int maxPathSum(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list, true);
+        System.out.println(res);
+        return max;
+    }
+    private int helper(TreeNode node, List<Integer> list, boolean isLeft) {
+        if (node == null) return 0;
+        
+        List<Integer> leftList = new ArrayList<>();
+        List<Integer> rightList = new ArrayList<>();
+        int left = Math.max(0, helper(node.left, leftList, true));
+        int right = Math.max(0, helper(node.right, rightList, false));
+        
+        if (left >= right) {
+            list.addAll(leftList);
+        } else {
+            list.addAll(rightList);
+        }
+        
+        if (isLeft)
+            list.add(node.val);
+        else
+            list.add(0, node.val);
+        
+        if (max < left + right + node.val) {
+            max = left + right + node.val;
+            res = new ArrayList<>();
+            res.addAll(leftList);
+            res.add(node.val);
+            res.addAll(rightList);
+            
+        }
+        // max = Math.max(max, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
