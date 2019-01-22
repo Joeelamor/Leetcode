3ab01d75799e215ec147a3ce2bc8eaacceeced4c
Joeelamor <lizhongzhang330@gmail.com>
2019-01-22 12:35:18 -0800

    Add a new method

--- java/138.CopyListwithRandomPointer.java
+++ java/138.CopyListwithRandomPointer.java
@@ -27,3 +27,49 @@ public class Solution {
         return map.get(head);
     }
 }
+
+// Method 2: O(1) space, O(n) time
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode iter = head, next;
+        // 1. insert copy to the original list
+        while (iter != null) {
+            next = iter.next;
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+            iter = next;
+        }
+        // 2. copy random
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null)
+                iter.next.random = iter.random.next;
+            iter = iter.next.next;
+        }
+        // 3. extract copy list from list
+        iter = head;
+        RandomListNode newHead = new RandomListNode(0);
+        RandomListNode copy, copyIter = newHead;
+        while (iter != null) {
+            next = iter.next.next;
+            // extract
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+            
+            iter.next = next;
+            iter = next;
+        }
+        return newHead.next;
+    }
+}
