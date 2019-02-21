a9d785ec58571e1358a6b4546ce8b15c266334ca
Joeelamor <lizhongzhang330@gmail.com>
2019-02-20 16:16:57 -0800

    Question 176

---
+++ sql/176.SecondHighestSalary.sql
@@ -0,0 +1,6 @@
+# 176 Second Highest Salary
+
+# Write your MySQL query statement below
+select max(Salary) as SecondHighestSalary
+from Employee
+where Salary not in (select max(Salary) from Employee)
