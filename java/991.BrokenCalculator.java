1a61c11f7d476107188391c2d99dbb29a5f44331
Joeelamor <lizhongzhang330@gmail.com>
2019-02-22 14:29:31 -0800

    Question 991

---
+++ java/991.BrokenCalculator.java
@@ -0,0 +1,15 @@
+// Question 991 Broken Calculator
+
+class Solution {
+    public int brokenCalc(int X, int Y) {
+        int res = 0;
+        while (Y > X) {
+            res++;
+            if (Y % 2 == 1)
+                Y++;
+            else
+                Y /= 2;
+        }
+        return res + X - Y;
+    }
+}
