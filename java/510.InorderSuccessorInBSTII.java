35c594e914c0eedbf65d0eba1a732676a7da8f9b
Joeelamor <lizhongzhang330@gmail.com>
2019-03-25 14:39:27 -0700

    Question 510

---
+++ java/510.InorderSuccessorInBSTII.java
@@ -0,0 +1,28 @@
+510 Inorder Successor in BST II
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+class Solution {
+    public Node inorderSuccessor(Node x) {
+        if (x.right == null) {
+            Node result = x.parent;
+            while (result != null && result.val < x.val) {
+                result = result.parent;
+            }
+            return result;
+        } else {
+            Node result = x.right;
+            while (result.left != null) {
+                result = result.left;
+            }
+            return result;
+        }
+    }
+}
